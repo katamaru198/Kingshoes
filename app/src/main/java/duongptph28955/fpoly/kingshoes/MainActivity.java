ba0f7d530a71e4ae3de7dcbe34cce9685fc4745e
ptd19 <duongptph28955@fpt.edu.vn>
2023-03-10 23:59:53 +0700

    Initial commit

---
+++ app/src/main/java/duongptph28955/fpoly/kingshoes/MainActivity.java
@@ -0,0 +1,14 @@
+package duongptph28955.fpoly.kingshoes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
