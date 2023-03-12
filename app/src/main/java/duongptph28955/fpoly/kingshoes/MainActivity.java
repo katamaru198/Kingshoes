48dc68708717a309c654c8369a8643f2313870e6
ptd19 <duongptph28955@fpt.edu.vn>
2023-03-12 13:04:43 +0700

    upload and update

--- app/src/main/java/duongptph28955/fpoly/kingshoes/MainActivity.java
+++ app/src/main/java/duongptph28955/fpoly/kingshoes/MainActivity.java
@@ -1,15 +1,94 @@
 package duongptph28955.fpoly.kingshoes;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    NavigationView nav;
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String dev = "1";
+        toolbar = findViewById(R.id.tool_bar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        nav = findViewById(R.id.navigationView);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            Fragment fragment;
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.action_LoaiGiay:
+
+                        break;
+                    case R.id.action_Giay:
+
+                        break;
+                    case R.id.action_mauSac:
+
+                        break;
+                    case R.id.action_size:
+
+                        break;
+                    case R.id.action_khachHang:
+
+                        break;
+                    case R.id.action_HoaDon:
+
+                        break;
+                    case R.id.action_top10:
+
+                        break;
+                    case R.id.action_doanhThu:
+
+                        break;
+                    case R.id.action_themTaiKhoan:
+
+                        break;
+                    case R.id.action_doiPass:
+
+                        break;
+                    case R.id.action_dangXuat:
+                        Intent intent1 = new Intent(MainActivity.this, DangNhapActivity.class);
+                        startActivity(intent1);
+                        break;
+                    default:
+                        break;
+                }
+
+                fragmentManager.beginTransaction().replace(R.id.linerLayout,fragment).commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                setTitle(item.getTitle());
+                return false;
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
